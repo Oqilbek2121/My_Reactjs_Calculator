c0a1d0c917095f053348e97d642d982059ca7ada
Oqilbek2121 <oqilbek851@gmail.com>
2023-09-11 21:39:58 +0500

    my_first_reactjs_calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [btnResult, setResult] = useState("");
+
+    const btnClick = (e) => {
+        setResult(btnResult.concat(e.target.name));
+    }
+
+    const btnClear = () => {
+        setResult("");
+    }
+
+    const btnBackpace = () => {
+        setResult(btnResult.slice(0, btnResult.length - 1));
+    }
+
+    const btnCalculator = () => {
+        try {
+            setResult(eval(btnResult).toString());
+        } catch(err) {
+            setResult("Calculate error!");
+        }
+    }
+    return (
+        <div className='container'>
+            <div>
+                <form>
+                    <input type="text" value={btnResult} />
+                </form>
+
+                <div className='keywords'>
+                    <div className='col'>
+                        <button onClick={btnClear} className='btn btn-outline-primary' id="clear">Clear</button>
+                        <button onClick={btnBackpace} className='btn btn-outline-primary' id="backpace">C</button>
+                        <button name="/" onClick={btnClick} className='btn btn-outline-primary'>&divide;</button>
+                    </div>
+
+                    <div className='col'>
+                        <button name="7" onClick={btnClick} className='btn btn-outline-dark'>7</button>
+                        <button name="8" onClick={btnClick} className='btn btn-outline-dark'>8</button>
+                        <button name="9" onClick={btnClick} className='btn btn-outline-dark'>9</button>
+                        <button name="*" onClick={btnClick} className='btn btn-outline-primary'>&times;</button>
+                    </div>
+
+                    <div className='col'>
+                        <button name="4" onClick={btnClick} className='btn btn-outline-dark'>4</button>
+                        <button name="5" onClick={btnClick} className='btn btn-outline-dark'>5</button>
+                        <button name="6" onClick={btnClick} className='btn btn-outline-dark'>6</button>
+                        <button name="-" onClick={btnClick} className='btn btn-outline-primary'>&ndash;</button>
+                    </div>
+
+                    <div className='col'>
+                        <button name="1" onClick={btnClick} className='btn btn-outline-dark'>1</button>
+                        <button name="2" onClick={btnClick} className='btn btn-outline-dark'>2</button>
+                        <button name="3" onClick={btnClick} className='btn btn-outline-dark'>3</button>
+                        <button name="+" onClick={btnClick} className='btn btn-outline-primary'>+</button>
+                    </div>
+
+                    <div className='col'>
+                        <button name="0" onClick={btnClick} className='btn btn-outline-dark'>0</button>
+                        <button name="." onClick={btnClick} className='btn btn-outline-dark'>.</button>
+                        <button onClick={btnCalculator} className='btn btn-outline-primary' id="equal">=</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
